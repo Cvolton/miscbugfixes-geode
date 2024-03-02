0c7e5916bb8223255c7b7a217a8c5cf6207ca7d3
Cvolton <cvolton@cvolton.eu>
2024-03-02 21:42:22 +0100

    add geode updater fix

---
+++ src/GeodeUpdaterFix.cpp
@@ -0,0 +1,11 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+$execute {
+    auto mod = Loader::get()->getLoadedMod("geode.loader");
+    auto value = mod->getSavedValue<std::string>("last-modified-auto-update-check");
+    if (Loader::get()->getVersion() < VersionInfo::parse("2.0.0-beta.21")) {
+        mod->setSavedValue<std::string>("last-modified-auto-update-check", "Sat, 25 Feb 2023 15:16:52 GMT")
+    }
+}
