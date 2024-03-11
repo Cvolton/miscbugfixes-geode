3e8695933aac16c20b74005747d11ca85a787c04
Cvolton <cvolton@cvolton.eu>
2024-03-11 01:35:49 +0100

    bump latest geode ver

--- src/GeodeUpdaterFix.cpp
+++ src/GeodeUpdaterFix.cpp
@@ -5,7 +5,7 @@ using namespace geode::prelude;
 $execute {
     auto mod = Loader::get()->getLoadedMod("geode.loader");
     auto value = mod->getSavedValue<std::string>("last-modified-auto-update-check");
-    if (Loader::get()->getVersion() < VersionInfo::parse("2.0.0-beta.21")) {
+    if (Loader::get()->getVersion() < VersionInfo::parse("2.0.0-beta.22")) {
         mod->setSavedValue<std::string>("last-modified-auto-update-check", "Sat, 25 Feb 2023 15:16:52 GMT");
     }
 }
