b30c88fc1476ee20a264f009ce58fad76e8e1ffb
Cvolton <cvolton@cvolton.eu>
2024-03-16 02:17:38 +0100

    improve ccmenuitem activate fix

--- src/CCMenuItemActivateFix.cpp
+++ src/CCMenuItemActivateFix.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCMenuItem.hpp>
+#include <Geode/modify/CCMenu.hpp>
 
 using namespace geode::prelude;
 
@@ -7,6 +8,14 @@ class $modify(cocos2d::CCMenuItem) {
     void activate() {
         this->retain();
         cocos2d::CCMenuItem::activate();
+        this->release();
+    }
+};
+
+class $modify(cocos2d::CCMenu) {
+    void ccTouchEnded(CCTouch* touch, CCEvent* event) {
+        this->retain();
+        cocos2d::CCMenu::ccTouchEnded(touch, event);
         this->release();
     }
 };
