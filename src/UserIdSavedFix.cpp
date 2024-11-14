23e1ceb12e5a69d0d5b36e2466e28207a21d82b8
Cvolton <cvolton@cvolton.eu>
2024-11-14 18:39:07 +0100

    update useridsavedfix to 2.2074

--- src/UserIdSavedFix.cpp
+++ src/UserIdSavedFix.cpp
@@ -18,34 +18,34 @@ $execute {
             {0x74, 0x00}
         );
     #elif defined(GEODE_IS_INTEL_MAC)
-        static_assert(GEODE_COMP_GD_VERSION == 22060, "Wrong GD version detected");
+        static_assert(GEODE_COMP_GD_VERSION == 22074, "Wrong GD version detected");
         //patch JZ to jump by 0 bytes
         auto result = patch(
-            0x5660f3,
+            0x5488a6,
             {0x74, 0x1d},
             {0x74, 0x00}
         );
     #elif defined(GEODE_IS_ARM_MAC)
-        static_assert(GEODE_COMP_GD_VERSION == 22060, "Wrong GD version detected");
+        static_assert(GEODE_COMP_GD_VERSION == 22074, "Wrong GD version detected");
         // NOP out CBZ
         auto result = patch(
-            0x4b4238,
+            0x4a1140,
             {0x15, 0x01, 0x00, 0x34},
             {0x1F, 0x20, 0x03, 0xD5}
         );
     #elif defined(GEODE_IS_ANDROID32)
-        static_assert(GEODE_COMP_GD_VERSION == 22060, "Wrong GD version detected");
+        static_assert(GEODE_COMP_GD_VERSION == 22074, "Wrong GD version detected");
         // NOP out CBZ
         auto result = patch(
-            0x3B3FC4,
+            0x3B8F48,
             {0x56, 0xb1},
             {0x00, 0xbf}
         );
     #elif defined(GEODE_IS_ANDROID64)
-        static_assert(GEODE_COMP_GD_VERSION == 22060, "Wrong GD version detected");
+        static_assert(GEODE_COMP_GD_VERSION == 22074, "Wrong GD version detected");
         //patch CBNZ to B
         auto result = patch(
-            0x6E5FFC,
+            0x6EFF34,
             {0xf6, 0x30, 0x00, 0x35},
             {0x87, 0x01, 0x00, 0x14}
         );
