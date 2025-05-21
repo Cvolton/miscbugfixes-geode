eed0cfe9873149e0ff0d78bffd597dc9c2b465a7
Cvolton <cvolton@cvolton.eu>
2025-05-21 19:15:03 +0200

    fix level list refresh

--- src/ListsNotLoadingFix.cpp
+++ src/ListsNotLoadingFix.cpp
@@ -8,15 +8,20 @@ using namespace geode::prelude;
 class $modify(MBLevelListLayer, LevelListLayer) {
     struct Fields {
         bool m_isBeingFixed = false;
+        bool m_triedFixing = false;
         std::string m_key;
     };
+
+    void restoreOldSearchObject() {
+        m_fields->m_isBeingFixed = false;
+        m_searchObject->m_searchType = SearchType::LevelListsOnClick;
+        m_searchObject->m_searchQuery = std::to_string(m_levelList->m_listID);
+    }
 
     void loadLevelsFinished(cocos2d::CCArray* levels, char const* key, int type) {
         auto sv = std::string_view(key);
         if(sv.starts_with("26") && m_fields->m_isBeingFixed) {
-            m_fields->m_isBeingFixed = false;
-            m_searchObject->m_searchType = SearchType::LevelListsOnClick;
-            m_searchObject->m_searchQuery = std::to_string(m_levelList->m_listID);
+            restoreOldSearchObject();
             key = m_fields->m_key.c_str();
         }
 
@@ -26,19 +31,30 @@ class $modify(MBLevelListLayer, LevelListLayer) {
 
 class $modify(LevelBrowserLayer) {
     void loadLevelsFailed(char const* key, int type) {
+        if(auto self = typeinfo_cast<LevelListLayer*>(this)) {
+            auto sv = std::string_view(key);
+            auto selfFields = reinterpret_cast<MBLevelListLayer*>(self);
+
+            if(sv.starts_with("26") && selfFields->m_fields->m_isBeingFixed) {
+                selfFields->m_fields->m_isBeingFixed = false;
+                selfFields->restoreOldSearchObject();
+                selfFields->m_fields->m_triedFixing = true;
+                key = selfFields->m_fields->m_key.c_str();
+            }
+        }
+
         LevelBrowserLayer::loadLevelsFailed(key, type);
 
         if(auto self = typeinfo_cast<LevelListLayer*>(this)) {
             auto sv = std::string_view(key);
-            if(!sv.starts_with("25")) return;
+            auto selfFields = reinterpret_cast<MBLevelListLayer*>(self);
+            if(!sv.starts_with("25") || selfFields->m_fields->m_triedFixing) return;
 
             log::info("LevelListLayer::loadLevelsFailed: key = {}, type = {}", key, type);
             log::info("levelsString: {}", self->m_levelList->m_levelsString);
 
             m_searchObject->m_searchType = SearchType::Type26;
             m_searchObject->m_searchQuery = self->m_levelList->m_levelsString;
-
-            auto selfFields = reinterpret_cast<MBLevelListLayer*>(self);
 
             selfFields->m_fields->m_isBeingFixed = true;
             selfFields->m_fields->m_key = key;
