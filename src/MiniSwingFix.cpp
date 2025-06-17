c80ebb6f6d1ef06454b96703fe57c54d83c5fd76
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 13:12:32 -0400

    remove a line

--- src/MiniSwingFix.cpp
+++ src/MiniSwingFix.cpp
@@ -5,8 +5,7 @@ using namespace geode::prelude;
 class $modify(MyPlayerObject, PlayerObject) {
 	void togglePlayerScale(bool p0, bool p1) {
 		PlayerObject::togglePlayerScale(p0, p1);
-		GJBaseGameLayer* gjbgl = GJBaseGameLayer::get();
-		if (!gjbgl || (this != gjbgl->m_player1 && this != gjbgl->m_player2) || !this->m_isSwing) return;
+		if (!m_gameLayer || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2) || !m_isSwing) return;
 		PlayerObject::updatePlayerSwingFrame(GameManager::get()->getPlayerSwing());
 	}
 };
