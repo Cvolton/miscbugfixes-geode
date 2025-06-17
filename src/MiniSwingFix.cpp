7afe64392163426e36b0c5599454cc42422b986e
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 13:31:27 -0400

    waiT IM SILLY

--- src/MiniSwingFix.cpp
+++ src/MiniSwingFix.cpp
@@ -5,7 +5,7 @@ using namespace geode::prelude;
 class $modify(MyPlayerObject, PlayerObject) {
 	void togglePlayerScale(bool p0, bool p1) {
 		PlayerObject::togglePlayerScale(p0, p1);
-		if (!!m_isSwing || !m_defaultMiniIcon || !m_gameLayer || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2)) return;
+		if (!m_isSwing || !m_defaultMiniIcon || !m_gameLayer || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2)) return;
 		PlayerObject::updatePlayerSwingFrame(GameManager::get()->getPlayerSwing());
 	}
 };
