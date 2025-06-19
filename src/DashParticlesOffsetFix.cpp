99bff6172f25f0d2cfba85446326ca63f9e32fc9
ConfiG <cgytrus@cgyt.ru>
2025-06-19 13:30:33 +0300

    dash particles offset fix

---
+++ src/DashParticlesOffsetFix.cpp
@@ -0,0 +1,17 @@
+#include <Geode/modify/PlayerObject.hpp>
+#include "_Utils.hpp"
+
+using namespace geode::prelude;
+
+STATIC_BOOL_SETTING(fixDashParticlesOffset, fix-dash-particles-offset);
+
+class $modify(PlayerObject) {
+    $override void setPosition(const CCPoint& position) {
+        PlayerObject::setPosition(position);
+        // 2.2 changed this to the position of m_trailingParticles
+        // which are used for ship and ufo click particles
+        // this fix reverts this to the 2.113 behavior
+        if (s_fixDashParticlesOffset)
+            m_dashParticles->setPosition(position);
+    }
+};
