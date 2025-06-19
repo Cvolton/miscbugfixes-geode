7c54776cf493eeb5e00698d4008e29c8d84b371f
Cvolton <cvolton@cvolton.eu>
2025-06-19 11:46:32 +0200

    add mini swing setting

--- src/MiniSwingFix.cpp
+++ src/MiniSwingFix.cpp
@@ -1,11 +1,14 @@
 #include <Geode/modify/PlayerObject.hpp>
+#include "_Utils.hpp"
 
 using namespace geode::prelude;
+
+STATIC_BOOL_SETTING(fixMiniSwing, fix-mini-swing);
 
 class $modify(MyPlayerObject, PlayerObject) {
 	void togglePlayerScale(bool p0, bool p1) {
 		PlayerObject::togglePlayerScale(p0, p1);
-		if (!m_isSwing || !m_defaultMiniIcon || !m_gameLayer || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2)) return;
+		if (!m_isSwing || !m_defaultMiniIcon || !m_gameLayer || !s_fixMiniSwing || (this != m_gameLayer->m_player1 && this != m_gameLayer->m_player2)) return;
 		PlayerObject::updatePlayerSwingFrame(GameManager::get()->getPlayerSwing());
 	}
 };
