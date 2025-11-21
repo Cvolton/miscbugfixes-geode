5329041b621ca8659e16931e311c75f1fb14c2b1
ConfiG <cgytrus@cgyt.ru>
2025-11-21 10:09:46 +0300

    update: it breaks something (:

--- src/DashScaleFix.cpp
+++ src/DashScaleFix.cpp
@@ -12,19 +12,21 @@ class $modify(PlayerObject) {
     // your cube scale will end up still at 0.9
     // i assume robtop just forgot to call resetPlayerIcon in these two,
     // so lets hope calling it here doesnt break anything (:
+    // update: it breaks something, no more resetPlayerIcon call :D
 
     $override void toggleRobotMode(bool on, bool start) {
         if (on || !m_isRobot)
             return PlayerObject::toggleRobotMode(on, start);
         PlayerObject::toggleRobotMode(on, start);
-        this->resetPlayerIcon();
+        m_iconSprite->setScale(1.0f);
+        m_iconGlow->setScale(1.0f);
     }
 
     $override void toggleRollMode(bool on, bool start) {
         if (on || !m_isBall)
             return PlayerObject::toggleRollMode(on, start);
         PlayerObject::toggleRollMode(on, start);
-        this->resetPlayerIcon();
+        m_iconSprite->setScale(1.0f);
+        m_iconGlow->setScale(1.0f);
     }
 };
-
