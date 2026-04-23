41d18cc89461cd46c787229640a972703a8b41fe
Cvolton <cvolton@cvolton.eu>
2026-04-23 15:59:14 +0200

    add benchmark

--- src/AndroidSavingSpeedFix.cpp
+++ src/AndroidSavingSpeedFix.cpp
@@ -4,7 +4,6 @@
 
 using namespace geode::prelude;
 
-#ifdef GEODE_IS_ANDROID64
 
 static auto levelPatch = patch(0xB2A650, {0x01, 0x00, 0x80, 0x12}, {0x01, 0x00, 0x80, 0x52});
 
@@ -16,4 +15,36 @@ $on_mod(Loaded) {
     }
 }
 
-#endif
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        std::vector<ByteVector> bytes = {
+            {0x01, 0x00, 0x80, 0x52}, // zlib level 0
+            {0x21, 0x00, 0x80, 0x52},  // zlib level 1
+            {0x41, 0x00, 0x80, 0x52},  // zlib level 2
+            {0x61, 0x00, 0x80, 0x52},  // zlib level 3
+            {0x81, 0x00, 0x80, 0x52},  // zlib level 4
+            {0xa1, 0x00, 0x80, 0x52},  // zlib level 5
+            {0xc1, 0x00, 0x80, 0x52},  // zlib level 6
+            {0xe1, 0x00, 0x80, 0x52},  // zlib level 7
+            {0x01, 0x01, 0x80, 0x52},   // zlib level 8
+            {0x21, 0x01, 0x80, 0x52}    // zlib level 9
+        };
+        
+        size_t idx = 0;
+        for(const auto& byteVec : bytes) {
+            levelPatch.unwrap()->updateBytes(byteVec);
+
+            auto now = asp::Instant::now();
+            GameManager::get()->save();
+            LocalLevelManager::get()->save();
+            log::info("Saving with zlib level {} took {}", idx, now.elapsed());
+
+            idx++;
+        }
+
+        return true;
+    }
+}
