bd29a154ca2c9f6b45e8eb8220b292729c9584b3
Cvolton <cvolton@cvolton.eu>
2026-04-23 15:30:01 +0200

    make it a patch instead

--- src/AndroidSavingSpeedFix.cpp
+++ src/AndroidSavingSpeedFix.cpp
@@ -1,35 +1,10 @@
-#ifdef GEODE_IS_ANDROID64
 
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-void deflateInitHook(z_streamp strm, int  level, int  method,
-                 int windowBits, int memLevel,
-                 int strategy, const char *version,
-                 int stream_size) {
-    
-    log::info("Deflate init called with level {}, method {}, windowBits {}, memLevel {}, strategy {}, version {}, stream_size {}",
-        level, method, windowBits, memLevel, strategy, version, stream_size);
-    level = 1;
-    auto result = deflateInit2(strm, level, method, windowBits, memLevel, strategy);
-}
+#ifdef GEODE_IS_ANDROID64
 
-$on_mod(Loaded) {
-    Mod::get()->hook(
-        reinterpret_cast<void*>(
-            geode::addresser::getNonVirtual(
-                geode::modifier::Resolve<z_streamp strm, int  level, int  method,
-                                      int windowBits, int memLevel,
-                                      int strategy, const char *version,
-                                      int stream_size>::func(&deflateInit2_)
-            )
-        ),
-        &deflateInitHook,
-        "deflateInit2_",
-        tulip::hook::TulipConvention::Cdecl
-    );
-
-}
+static auto patch = patch(0xB2A650, {0x01, 0x00, 0x80, 0x12}, {0x21, 0x00, 0x80, 0x52});
 
 #endif
