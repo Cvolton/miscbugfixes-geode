17c1a6b3ba72af8cc6360fac4c9509ccceb52bfe
Cvolton <cvolton@cvolton.eu>
2026-05-12 23:29:05 +0200

    fix start pos mirror mode taking precedence over practice checkpoints (closes #40)

--- src/StartPosMirrorModeFix.cpp
+++ src/StartPosMirrorModeFix.cpp
@@ -8,7 +8,7 @@ STATIC_BOOL_SETTING(enforceStartPosMirrorMode, enforce-start-pos-mirror-mode);
 class $modify(PlayLayer) {
 	void resetLevel() {
 		PlayLayer::resetLevel();
-		if (!s_enforceStartPosMirrorMode || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings) return;
+		if (!s_enforceStartPosMirrorMode || !m_isTestMode || !m_startPosObject || !m_startPosObject->m_startSettings || m_isPracticeMode) return;
 		PlayLayer::toggleFlipped(m_startPosObject->m_startSettings->m_mirrorMode, true);
 	}
 };
