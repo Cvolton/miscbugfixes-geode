cca5e724d67e9af01071c62d5fe9d5474c250557
Cvolton <cvolton@cvolton.eu>
2026-05-13 04:43:20 +0200

    remove unused menulayer in thread queue functionality

--- src/_MenuLayerManager.cpp
+++ src/_MenuLayerManager.cpp
@@ -5,14 +5,9 @@
 using namespace geode::prelude;
 
 static std::vector<std::function<void(MenuLayer*)>> s_functionQueue;
-static std::vector<std::function<void(MenuLayer*)>> s_functionQueueInThread;
 
 void MenuLayerManager::queueFunction(std::function<void(MenuLayer*)> func) {
     s_functionQueue.push_back(func);
-}
-
-void MenuLayerManager::queueFunctionInThread(std::function<void(MenuLayer*)> func) {
-    s_functionQueueInThread.push_back(func);
 }
 
 #include <Geode/modify/MenuLayer.hpp>
@@ -24,16 +19,6 @@ class $modify(MenuLayer) {
             func(this);
         }
         s_functionQueue.clear();
-
-        //disabled for now
-        /*std::thread([this] {
-            thread::setName("MenuLayerManager");
-
-            for(auto& func : s_functionQueueInThread) {
-                func(this);
-            }
-            s_functionQueueInThread.clear();
-        }).detach();*/
 
         return true;
     }
